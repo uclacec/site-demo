0bc9818d6c5098bf9c00bc7c9dd9cb402cd1fabe
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2017-05-25 13:45:16 -0700

    Change opacity with button click

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,19 +1,35 @@
 console.log("hello")
-
+/*
 $('li a').click(function(){
 	console.log("button clicked")
- 	if (!$(this).hasClass("is-active")) {
-  		$("li.is-active ").removeClass("is-active");
+ 	if (!$(this).hasClass("is-active") || ) {
+        $("isactive").addClass("inactive");
+  		$("isactive").removeClass("isactive");
   		$(this).parent().addClass("is-active");
   		console.log($(this))
  	}
 });
-
+*/
 $('.speakers-button').click(function(e) {
+    $(".isactive").removeClass("isactive");
+    $(".s").removeClass("inactive");
+    $(".fi").addClass("inactive");
+    $(".c").addClass("inactive");
+    $(".s").addClass("isactive");
 });
 
 $('.films-button').click(function(e) {
+    $(".isactive").removeClass("isactive");
+    $(".fi").removeClass("inactive");
+    $(".s").addClass("inactive");
+    $(".c").addClass("inactive");
+    $(".fi").addClass("isactive");
 });
 
 $('.concerts-button').click(function(e) {
+    $(".isactive").removeClass("isactive");
+    $(".c").removeClass("inactive");
+    $(".fi").addClass("inactive");
+    $(".s").addClass("inactive");
+    $(".c").addClass("isactive");
 });
