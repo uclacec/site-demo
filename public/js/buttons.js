f0cb1c6b9f1817f1e6be368ff2e812d781614c62
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2017-05-25 13:47:07 -0700

    revert changes

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,35 +1,19 @@
 console.log("hello")
-/*
+
 $('li a').click(function(){
 	console.log("button clicked")
- 	if (!$(this).hasClass("is-active") || ) {
-        $("isactive").addClass("inactive");
-  		$("isactive").removeClass("isactive");
+ 	if (!$(this).hasClass("is-active")) {
+  		$("li.is-active ").removeClass("is-active");
   		$(this).parent().addClass("is-active");
   		console.log($(this))
  	}
 });
-*/
+
 $('.speakers-button').click(function(e) {
-    $(".isactive").removeClass("isactive");
-    $(".s").removeClass("inactive");
-    $(".fi").addClass("inactive");
-    $(".c").addClass("inactive");
-    $(".s").addClass("isactive");
 });
 
 $('.films-button').click(function(e) {
-    $(".isactive").removeClass("isactive");
-    $(".fi").removeClass("inactive");
-    $(".s").addClass("inactive");
-    $(".c").addClass("inactive");
-    $(".fi").addClass("isactive");
 });
 
 $('.concerts-button').click(function(e) {
-    $(".isactive").removeClass("isactive");
-    $(".c").removeClass("inactive");
-    $(".fi").addClass("inactive");
-    $(".s").addClass("inactive");
-    $(".c").addClass("isactive");
 });
