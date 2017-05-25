be548d5349e34ca26d5c8560e9980cf09e83a644
Garchbold <garchbold@g.ucla.edu>
2017-05-25 13:03:03 -0700

    button active function

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,11 +1,19 @@
+console.log("hello")
+
+$('li a').click(function(){
+	console.log("button clicked")
+ 	if (!$(this).hasClass("is-active")) {
+  		$("li.is-active ").removeClass("is-active");
+  		$(this).parent().addClass("is-active");
+  		console.log($(this))
+ 	}
+});
+
 $('.speakers-button').click(function(e) {
-	$(modal).fadeOut(200);
 });
 
-$('.films-button.).click(function(e) {
-	$(modal).fadeOut(200);
+$('.films-button').click(function(e) {
 });
 
-$('.concerts-button.).click(function(e) {
-	$().fadeOut(200);
+$('.concerts-button').click(function(e) {
 });
