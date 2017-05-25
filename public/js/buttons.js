984472bb577e88c2f7d4bdf2f01aa6a2c5adeb7c
Garchbold <garchbold@g.ucla.edu>
2017-05-25 12:24:04 -0700

    adding button functions

---
+++ public/js/buttons.js
@@ -0,0 +1,11 @@
+$(speakersbutton).click(function(e) {
+	$(modal).fadeOut(200);
+});
+
+$(filmsbutton).click(function(e) {
+	$(modal).fadeOut(200);
+});
+
+$(concertsbutton).click(function(e) {
+	$().fadeOut(200);
+});
